66ca74f43514b503d55aa301b2066a6c1385dd79
anhvo <anh.vo@watasoftware.com>
2025-01-16 12:50:13 +0700

    fix run

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Lắng nghe tất cả địa chỉ network
-    port: 5173  // Port mặc định của Vite
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
